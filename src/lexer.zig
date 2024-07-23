8c1ea74fbaa511acf1f266de2fc3c7ff47bbecf4
Lawson Cale <lawson@lawsmat.com>
2024-07-23 19:01:53 -0400

    not working now, but added structure

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 
 const GMA_DECL = "GOOD MORNING AMERICA!!!";
+const NUMBERS = "1234567890";
+const IDENTIFIER_CHARS = "abcdefghijklmnopqrstuvwxyz_" ++ NUMBERS;
 
-pub const Token = union(enum) { good_morning_america, proc, t_var, out, lparen, rparen, number: f64, string: []u8 };
+pub const Token = union(enum) { good_morning_america, proc, t_var, out, lparen, rparen, number: f64, string: []u8, eof };
 pub const LexerError = error{ DidNotSayGoodMorning, UnexpectedCharacter };
 
 pub const Lexer = struct {
@@ -10,10 +12,25 @@ pub const Lexer = struct {
     source: []u8,
     alloc: std.mem.Allocator,
 
+    pub fn new(alloc: std.mem.Allocator, source: []u8) @This() {
+        return @This(){
+            .at = 0,
+            .alloc = alloc,
+            .source = source,
+        };
+    }
+
     pub fn lex(self: *Lexer) !std.ArrayList(Token) {
         var tok = std.ArrayList(Token).init(self.alloc);
         // expect gma
         try tok.append(try gma(self));
+
+        while (true) {
+            const cur = try next(self);
+            if (cur == Token.eof) break;
+            try tok.append(cur);
+        }
+
         return tok;
     }
 
@@ -25,11 +42,21 @@ pub const Lexer = struct {
         }
     }
 
-    pub fn new(alloc: std.mem.Allocator, source: []u8) @This() {
-        return @This(){
-            .at = 0,
-            .alloc = alloc,
-            .source = source,
-        };
+    fn next(self: *Lexer) !Token {
+        if (self.at > self.source.len) {
+            return Token.eof;
+        }
+        const curr = self.source[self.at];
+        switch (curr) {
+            '(' => {
+                return Token.lparen;
+            },
+            ')' => {
+                return Token.rparen;
+            },
+            else => {
+                return error.UnexpectedCharacter;
+            },
+        }
     }
 };
